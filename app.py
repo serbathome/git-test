a290425cad05551b03e113650e0c2cb644d00363
Sergei Biryukov <svbiryukov@luxoft.com>
2023-03-22 11:35:58 +0100

    added app.py

--- app.py
+++ app.py
@@ -5,5 +5,14 @@ class SomeTestClass:
     def __str__(self):
         return str(self.version)
 
+class SomeClass:
+    def __init__(self):
+	self.version = "0"
+    def __str__(self):
+	return self.version
+
 a = SomeTestClass()
 print(a)
+o = SomeClass()
+print(o)
+
